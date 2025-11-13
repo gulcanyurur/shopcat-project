6b859bb62a1f2ecae9eb1d0ceb403834cf5ff70f
Gulcan Yurur <134098427+gulcanyurur@users.noreply.github.com>
2025-11-13 12:52:46 +0300

    Fix Component path for Linux

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
+import "./index.css";import App from "./Components/App";
 
 
 createRoot(document.getElementById('root')!).render(
